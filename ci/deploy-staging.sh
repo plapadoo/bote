2d0068702b7ad0f521c55682ec375cd4e9e34397
Philipp Middendorf <middendorf@plapadoo.de>
2018-06-01 15:48:26 +0200

    CI: Don’t use “sudo docker”

--- ci/deploy-staging.sh
+++ ci/deploy-staging.sh
@@ -4,9 +4,9 @@
 set -eu
 set -o pipefail
 
-sudo docker build -t "$HOST_REGISTRY/bote:latest" .
-sudo docker login -u "$USER_REGISTRY" -p "$PW_REGISTRY" "$HOST_REGISTRY"
-sudo docker push "$HOST_REGISTRY/bote:latest"
+docker build -t "$HOST_REGISTRY/bote:latest" .
+docker login -u "$USER_REGISTRY" -p "$PW_REGISTRY" "$HOST_REGISTRY"
+docker push "$HOST_REGISTRY/bote:latest"
 ssh "$SSH_USER@$HOST_WWW" "docker rm -f bote || true"
 ssh "$SSH_USER@$HOST_WWW" "docker rmi $HOST_REGISTRY/bote:latest || true"
 ssh "$SSH_USER@$HOST_WWW" "docker login -u $USER_REGISTRY -p '$PW_REGISTRY' $HOST_REGISTRY"
