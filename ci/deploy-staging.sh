09409ebf650c202624b9c9ea86d8f3f0157132b1
Kim Rinnewitz <krinnewitz@plapadoo.de>
2018-09-19 17:37:31 +0200

    Deploy multiple instances of bote

--- ci/deploy-staging.sh
+++ ci/deploy-staging.sh
@@ -7,7 +7,12 @@ set -o pipefail
 docker build -t "$HOST_REGISTRY/bote:latest" .
 docker login -u "$USER_REGISTRY" -p "$PW_REGISTRY" "$HOST_REGISTRY"
 docker push "$HOST_REGISTRY/bote:latest"
-ssh "$SSH_USER@$HOST_WWW" "docker rm -f bote || true"
+ssh "$SSH_USER@$HOST_WWW" "docker rm -f $INSTANCES || true"
 ssh "$SSH_USER@$HOST_WWW" "docker rmi $HOST_REGISTRY/bote:latest || true"
 ssh "$SSH_USER@$HOST_WWW" "docker login -u $USER_REGISTRY -p '$PW_REGISTRY' $HOST_REGISTRY"
-ssh "$SSH_USER@$HOST_WWW" "docker run --publish 12649:8080 --volume /srv/bote-monica:/etc/bote --volume /srv/bote-monica:/var/lib/bote --name=bote --restart=always --detach $HOST_REGISTRY/bote"
+port=12649
+for instance in $INSTANCES
+do
+  ssh "$SSH_USER@$HOST_WWW" "docker run --publish $port:8080 --volume /srv/$instance:/etc/bote --volume /srv/$instance:/var/lib/bote --name=$instance --restart=always --detach $HOST_REGISTRY/bote"
+  ((port++))
+done
